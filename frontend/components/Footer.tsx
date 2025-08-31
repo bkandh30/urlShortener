4202fea2fee86bb25f7165b8394d8dfd31de1d4e
bkandh30 <kandhari.bhavya20@gmail.com>
2025-08-30 20:34:07 -0700

    Updated Footer section

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -7,9 +7,31 @@ export default function Footer() {
 		<footer className="w-full border-t bg-card/30 backdrop-blur-sm mt-auto">
 			<div className="container mx-auto px-4 py-8">
 				<div className="text-center">
-					<p className="text-sm text-muted-foreground">
-						© {currentYear} Hash.ly. Built with Next.js, Express, TypeScript
-						and TailwindCSS.
+					<p className="text-sm text-muted-foreground whitespace-nowrap">
+						{/* First Span */}
+						<span>© {currentYear} Hash.ly</span>
+
+						<span className="mx-2">|</span>
+
+						{/* Second Span */}
+						<span>
+							Built with Next.js, Node.js, Express, TypeScript and TailwindCSS
+						</span>
+
+						<span className="mx-2">|</span>
+
+						{/* Third Span */}
+						<span>
+							Developed by{" "}
+							<a
+								href="https://github.com/bkandh30/urlShortener"
+								target="_blank"
+								rel="noopener noreferrer"
+								className="font-medium text-blue-600 hover:underline"
+							>
+								Bhavya Kandhari
+							</a>
+						</span>
 					</p>
 				</div>
 			</div>
