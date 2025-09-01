c6b37ad3086e7ed725f84466c60d046a9bfd2b7f
bkandh30 <kandhari.bhavya20@gmail.com>
2025-08-31 18:27:37 -0700

    Revert "Updated monorepo version"
    This reverts commit dd9123b66de133bd00a71583ed4f5ff88d064dc5.

--- backend/dist/lib/id.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function makeShortId(existsFn: (id: string) => Promise<boolean>, maxRetries?: number): Promise<string>;
-//# sourceMappingURL=id.d.ts.map
--- backend/dist/lib/prisma.d.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "../generated/prisma/index.js";
-declare global {
-    var prisma: PrismaClient | undefined;
-}
-export declare const prisma: PrismaClient<import("../generated/prisma/index.js").Prisma.PrismaClientOptions, never, import("../generated/prisma/runtime/library.js").DefaultArgs>;
-export default prisma;
-//# sourceMappingURL=prisma.d.ts.map
